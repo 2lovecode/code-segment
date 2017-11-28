d3743605d1363ad2d398616d58d4a70283a595f4
liuhao <liu546hao@163.com>
2017-11-28 15:03:51 +0800

    基数排序

---
+++ PhpCodes/RadixSort/RadixSort.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Class RadixSort
+ *
+ * 基数排序法
+ */
+class RadixSort
+{
+    public $originalData = [];
+
+    public $resault = [];
+
+    public function __construct(array $originalData = [])
+    {
+        $this->originalData = $originalData;
+    }
+
+    public function sort()
+    {
+        $maxLen = $this->caculateMaxDigits();
+
+        $tmpData = $this->originalData;
+        $bucketData = [];
+
+
+        for($times = 0; $times < $maxLen; $times++) {
+            $t = pow(10, $times);
+            foreach ($tmpData as $value) {
+                $tmp = $value/$t;
+                $flag = $tmp%10;
+                $bucketData[$flag][] = $value;
+            }
+            $tmpData = [];
+
+            ksort($bucketData);
+            foreach ($bucketData as $value) {
+                foreach ($value as $v) {
+                    array_push($tmpData, $v);
+                }
+            }
+            $bucketData = [];
+        }
+
+        $this->resault = $tmpData;
+        return $this->resault;
+    }
+
+    public function caculateMaxDigits()
+    {
+        $maxLen = 0;
+        foreach ($this->originalData as $key => $value) {
+            $now = strlen($value);
+            if ($now > $maxLen) {
+                $maxLen = $now;
+            }
+        }
+
+        return $maxLen;
+    }
+}
+
+$example = new RadixSort([20, 67, 123, 111, 2, 34, 1, 78, 0, 33, 11, 15, 83]);
+echo '<pre>';
+var_dump($example->sort());
