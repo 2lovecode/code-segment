842525e04ce6fd1db6118d5636baf686f5c6ebad
liuhao <liu546hao@163.com>
2018-04-08 14:40:32 +0800

    tencent map javascript api used

--- PhpCodes/PhpSyntaxTestCode/FileOperation.php
+++ PhpCodes/PhpSyntaxTestCode/FileOperation.php
@@ -1,4 +1,25 @@
 <?php
+/**
+基于下面的问题写的代码：
+
+	有两个文本文件 A.txt B.txt
+	A.txt 3000万行，userid唯一，userid和username以空格分隔，如下所示：
+	userid	username
+	1		yi
+	2		er
+	3		san
+	...		...
+	B.txt 3000万行，userid唯一，userid和realname以空格分隔，如下所示：
+	userid	realname
+	1		一
+	2		二
+	3		三
+	...		...
+	请写一段代码，将B.txt中userid对应的username在A.txt里找出来，填充到B.txt的第三列，并给出时间复杂度。
+
+
+我们默认两个文件的行数据是一一对应的，即A的第n行数据对应B的第n行数据，这样我们的程序的时间复杂度是O(n)。
+*/
 header("content-type:text/html;charset=utf-8");
 
 function decodeLine(string $lineData, string $delimiter = null)
@@ -23,17 +44,16 @@ $testA = fopen('./TestData/FileOperation/testA.txt', 'r');
 $testB = fopen('./TestData/FileOperation/testB.txt', 'r+');
 
 $tmpFile = tmpfile();
-
+//
 while (($bBuffer = fgets($testB)) != false) {
 	$bList = decodeLine(trim($bBuffer, "\n\r"));
 	$tmpList = $bList;
-	while(($aBuffer = fgets($testA)) != false) {
+	if (($aBuffer = fgets($testA)) != false) {
 		$aList = decodeLine(trim($aBuffer, "\n\r"));
 		if ($aList[0] == $bList[0]) {
 			$strEncoding = mb_detect_encoding($aList[1], ['ASCII', 'UTF-8', 'GB2312']);
 			$resStr = mb_convert_encoding($aList[1], 'UTF-8', $strEncoding);
 			array_push($tmpList, $resStr."\n");
-			break;
 		}
 	}
 	fwrite($tmpFile, encodeLine($tmpList));
--- PhpCodes/PhpSyntaxTestCode/GetFileLastNumRow.php
+++ PhpCodes/PhpSyntaxTestCode/GetFileLastNumRow.php
@@ -1,4 +1,11 @@
 <?php
+/**
+基于以下问题，写此代码：
+	用php写一个函数，获取一个文本文件最后$n行内容，要求尽可能效率更高，并可以跨平台使用
+
+我理解的可以跨平台使用，是说的文件在windows平台和linux平台的行结束符不一致问题，在代码中我们没有体现这种不同。
+全部是在linux系统下的代码。
+*/
 header("content-type:text/html;charset=utf-8");
 
 class GetFileLastNumRow
