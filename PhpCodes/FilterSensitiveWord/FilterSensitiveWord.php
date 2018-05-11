ca4e1044c2778d957c44d4a8e54dae541ad1b481
liuhao <liu546hao@163.com>
2018-05-11 18:51:58 +0800

    DNF左侧全量匹配

--- PhpCodes/FilterSensitiveWord/FilterSensitiveWord.php
+++ PhpCodes/FilterSensitiveWord/FilterSensitiveWord.php
@@ -4,15 +4,37 @@ header("Content-type:text/html; charset=utf-8");
 
 class MyMap
 {
+    private $keySpace = [];
+
 	public function get($key)
 	{
-		return isset($this->$key) ? $this->$key : null;
+		return isset($this->keySpace[$key]) ? $this->keySpace[$key] : null;
 	}
 
 	public function put($key, $value)
 	{
-		$this->$key = $value;
+        $this->keySpace[$key] = $value;
 	}
+
+	public function hasNext()
+    {
+        return !empty($this->keySpace);
+    }
+}
+
+class MyStack
+{
+    private $stack = [];
+
+    public function push($value)
+    {
+        array_push($this->stack, $value);
+    }
+
+    public function pop()
+    {
+        return empty($this->stack) ? null : array_pop($this->stack);
+    }
 }
 
 class MyFilter
@@ -51,7 +73,7 @@ class MyFilter
 		$this->map = $tmp;
 	}
 
-	//最大匹配
+	//最大匹配(有问题)
 	public function searchFromMap($string)
 	{
 		$len = mb_strlen($string);
@@ -66,7 +88,9 @@ class MyFilter
 			if (!is_null($nowMap)) {
 				$str .= $nowWord;
 				if ($nowMap->get('isEnd')) {
-					array_push($result, $str);
+                    if (!in_array($str, $result)) {
+                        array_push($result, $str);
+                    }
 					$str = '';
 					$map = $tmp;
 				} else {
@@ -82,6 +106,49 @@ class MyFilter
 		}
 		return $result;
 	}
+
+	//左侧全量匹配
+    public function leftFullSearch($string)
+    {
+        $len = mb_strlen($string);
+        $tmp = $this->map;
+        $map = $this->map;
+        $str = '';
+        $result = [];
+        for ($i = 0; $i < $len; $i++) {
+            $nowWord = mb_substr($string, $i, 1);
+            $nowMap = $map->get($nowWord);
+
+            if (!is_null($nowMap)) {
+                $str .= $nowWord;
+                if ($nowMap->get('isEnd')) {
+                    if (!in_array($str, $result)) {
+                        array_push($result, $str);
+                    }
+                    if ($nowMap->hasNext()) {
+                        $map = $nowMap;
+                    } else {
+                        $str = '';
+                        $map = $tmp;
+                    }
+                } else {
+                    $map = $nowMap;
+                }
+            } else {
+                if (!empty($str)) {
+                    $i--;
+                }
+                $str = '';
+                $map = $tmp;
+            }
+        }
+        return $result;
+    }
+
+    public function matchSearch($string)
+    {
+
+    }
 }
 
 $example = new MyFilter();
@@ -91,6 +158,7 @@ $example->addWordToMap('中国男人');
 $example->addWordToMap('中国女人');
 $example->addWordToMap('中国男');
 $example->addWordToMap('中国女');
+$example->addWordToMap('中国');
 $example->addWordToMap('男人');
 $example->addWordToMap('女人');
 $example->addWordToMap('男');
@@ -100,6 +168,8 @@ $example->addWordToMap('人');
 
 $str = '我是中国人,我爱中国,中国男人发送中国女人男到中国男人发送';
 
-$result = $example->searchFromMap($str);
+var_dump($example->searchFromMap($str));
+
 //var_dump($example->map);
-var_dump($result);
+//var_dump($result);
+var_dump($example->leftFullSearch($str));
