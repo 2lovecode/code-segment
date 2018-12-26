2e98284561d57d5f7c1d4f4fee71179542be0b3a
liuhao <liu546hao@163.com>
2018-12-26 12:00:40 +0800

    MyStackTest单元测试

--- PhpCodes/Tests/InterestingQuestion/Stack/StackWithGetMin/MyStackTest.php
+++ PhpCodes/Tests/InterestingQuestion/Stack/StackWithGetMin/MyStackTest.php
@@ -19,4 +19,55 @@ class MyStackTest extends BaseTestCase
         return $myStack;
     }
 
+    /**
+     * @param $value
+     * @param $result
+     * @param MyStack $myStack
+     * @dataProvider pushAndGetMinProvider
+     * @depends testIsEmpty
+     */
+    public function testPushAndGetMin($value, $result, MyStack $myStack)
+    {
+        $myStack->push($value);
+        $this->assertEquals($result, $myStack->getMin());
+    }
+
+    public function pushAndGetMinProvider()
+    {
+        return [
+            'one'   => [200, 200],
+            'two'   => [150, 150],
+            'three' => [165, 150],
+            'four'  => [22, 22],
+            'five'  => [34, 22],
+            'six'   => [11, 11],
+        ];
+    }
+
+    /**
+     * @param $myStack
+     * @param $expect
+     * @param $value
+     * @dataProvider popAndGetMinProvider
+     * @depends testIsEmpty
+     * @depends testPushAndGetMin
+     */
+    public function testPopAndGetMin($expect, $value, MyStack $myStack)
+    {
+        $this->assertEquals($expect, $myStack->getMin());
+        $current = $myStack->pop();
+        $this->assertEquals($value, $current);
+    }
+
+    public function popAndGetMinProvider()
+    {
+        return [
+            'one'   => [11, 11],
+            'two'   => [22, 34],
+            'three' => [22, 22],
+            'four'  => [150, 165],
+            'five'  => [150, 150],
+            'six'   => [200, 200]
+        ];
+    }
 }
